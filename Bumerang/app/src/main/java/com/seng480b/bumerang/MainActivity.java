fded7548a81bd6d2e853ef9e5d0a7ea3e2f28cb2
Brady Schnell <bradyschnell@gmail.com>
2016-09-28 22:55:52 -0700

    Added MainActivity and added Facebook login button.

---
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/MainActivity.java
@@ -0,0 +1,17 @@
+package com.seng480b.bumerang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        AppEventsLogger.activateApp(this);
+    }
+}
