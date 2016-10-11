7150646d45ccc3d59e4c46a5b2b18297dec5e4ca
Conner <connerleverett@gmail.com>
2016-10-11 14:21:45 -0700

    Added compile lines to app and project level build files

--- Bumerang/app/src/main/java/com/seng480b/bumerang/CreateProfile.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/CreateProfile.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class CreateProfile extends AppCompatActivity {
-	private FirebaseAnalytics mFirebaseAnalytics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
