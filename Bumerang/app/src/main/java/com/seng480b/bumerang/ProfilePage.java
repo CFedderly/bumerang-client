66ea57ebf69aa11f297e40172735120c64a0bbe7
dtempleman <daniel.templeman@gmail.com>
2016-10-11 16:32:21 -0700

    made xml file for the profile page

---
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/ProfilePage.java
@@ -0,0 +1,18 @@
+package com.seng480b.bumerang;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+
+}
