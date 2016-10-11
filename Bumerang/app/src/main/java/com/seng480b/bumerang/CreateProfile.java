12bc7c620e9e6f82ce878a4527ce24cddbc5ce7d
Conner <connerleverett@gmail.com>
2016-10-11 14:46:08 -0700

    Added an analytics bundle to 'CreateProfile' to keep track of what people are saying they are able to lend out

--- Bumerang/app/src/main/java/com/seng480b/bumerang/CreateProfile.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/CreateProfile.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.analytics.FirebaseAnalytics.Event;
+import com.google.firebase.analytics.FirebaseAnalytics.Param;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -21,6 +24,16 @@ public class CreateProfile extends AppCompatActivity {
     public void goButton(View view) {
         Intent forward = new Intent(this, NavDrawer.class); // NavDrawer is the "home"
         // Get input from all the text fields here.
+        // This would be collecting data on what people are willing to lend out
+        Bundle itemsUserIsWillingToLendOut = new Bundle();
+        itemsUserIsWillingToLendOut.putString( Param.ITEM_ID, "USERS_ID" );
+        itemsUserIsWillingToLendOut.putString( Param.ITEM_CATEGORY, "OUT" );
+        //This would be the array of values which the user enters they are willing to lend out
+        String[] stringList = new String[10];
+        itemsUserIsWillingToLendOut.putStringArray( Param.VALUE,stringList );
+        FirebaseAnalytics itemsUserIsWillingToLendOutAnalytics = FirebaseAnalytics.getInstance( CreateProfile.this );
+        itemsUserIsWillingToLendOutAnalytics.logEvent( Event.SELECT_CONTENT, itemsUserIsWillingToLendOut );
+
         startActivity(forward);
     }
 }
