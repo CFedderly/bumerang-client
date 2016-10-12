90431aa1bdfd5a5f26d127ad2cae2312c24a3956
dtempleman <daniel.templeman@gmail.com>
2016-10-12 16:07:14 -0700

    changed profilepage activity to a fragment and added it to the nav bar

--- Bumerang/app/src/main/java/com/seng480b/bumerang/ProfilePage.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/ProfilePage.java
@@ -1,24 +1,22 @@
 package com.seng480b.bumerang;
 
-import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
-import android.widget.TextView;
 
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
+public class ProfilePage extends Fragment {
 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile_page, container, false);
 
-
-public class ProfilePage extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        
     }
+
 
 }
