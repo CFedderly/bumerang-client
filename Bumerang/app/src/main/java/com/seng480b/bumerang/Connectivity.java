37c909f8e60ceeaebd744133a95d6caf55fe2816
Charlotte Fedderly <charlottefedderly@gmail.com>
2016-10-12 01:20:57 -0700

    Started code for sending HTTP requests from the client side. Am able to get a basic response from the server right now

--- Bumerang/app/src/main/java/com/seng480b/bumerang/Connectivity.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/Connectivity.java
@@ -4,6 +4,14 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
 public class Connectivity {
 
     public static NetworkInfo getNetworkInfo(Context context) {
@@ -15,9 +23,44 @@ public class Connectivity {
     public static boolean checkNetworkConnection(Context context) {
         NetworkInfo netInfo = Connectivity.getNetworkInfo(context);
         if (netInfo != null) {
+            System.out.println(netInfo.isConnected());
             return netInfo.isConnected();
         } else {
             return false;
         }
+    }
+
+    public static String httpGet(String myUrl) throws IOException {
+        String result = "";
+        URL url = new URL(myUrl);
+        HttpURLConnection urlCon = (HttpURLConnection) url.openConnection();
+        urlCon.connect();
+        result = streamToString(urlCon.getInputStream());
+        return result;
+    }
+
+    public static String httpPut(String myUrl, String myJSON) throws IOException {
+        String result = "";
+        URL url = new URL(myUrl);
+        HttpURLConnection urlCon = (HttpURLConnection) url.openConnection();
+        urlCon.setRequestMethod("PUT");
+        OutputStreamWriter out = new OutputStreamWriter(urlCon.getOutputStream());
+        out.write(myJSON);
+        out.close();
+        result = streamToString(urlCon.getInputStream());
+        System.out.println(result);
+        return result;
+    }
+
+    private static String streamToString(InputStream inStream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
+        StringBuilder sb = new StringBuilder();
+        String inString;
+
+        while((inString = br.readLine()) != null) {
+            sb.append(inString);
+        }
+        br.close();
+        return sb.toString();
     }
 }
--- Bumerang/app/src/main/java/com/seng480b/bumerang/CreateProfile.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/CreateProfile.java
@@ -1,16 +1,32 @@
 package com.seng480b.bumerang;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class CreateProfile extends AppCompatActivity {
+    private static final String profileUrl = BuildConfig.SERVER_URL + "/profile/";
+    private Intent forward;
+    private Profile currProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+
+        currProfile = new Profile(
+                (EditText) findViewById(R.id.inputFirstName),
+                (EditText) findViewById(R.id.inputLastName),
+                (EditText) findViewById(R.id.inputAbout),
+                (EditText) findViewById(R.id.inputTags));
     }
 
     public void cancelButton(View view){
@@ -19,8 +35,49 @@ public class CreateProfile extends AppCompatActivity {
     }
 
     public void goButton(View view) {
-        Intent forward = new Intent(this, NavDrawer.class); // NavDrawer is the "home"
-        // Get input from all the text fields here.
-        startActivity(forward);
+        System.out.println(profileUrl);
+        forward = new Intent(this, NavDrawer.class); // NavDrawer is the "home"
+        if (Connectivity.checkNetworkConnection(getApplicationContext())) {
+            new CreateProfileTask().execute(profileUrl, currProfile.toJsonString());
+        }
+    }
+
+    private class CreateProfileTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return Connectivity.httpPut(params[0], params[1]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Unable to create profile. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            startActivity(forward);
+        }
+    }
+
+    public class Profile {
+        private EditText firstName;
+        private EditText lastName;
+        private EditText description;
+        private EditText tags;
+
+        public Profile(EditText firstName,
+                       EditText lastName,
+                       EditText description,
+                       EditText tags) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.description = description;
+            this.tags = tags;
+        }
+
+        public String toJsonString() {
+            JSONObject json = new JSONObject();
+            return "hello";
+        }
     }
 }
