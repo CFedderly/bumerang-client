476ac6eee2653676fb52d81e3d708ec79502b376
Brady Schnell <bradyschnell@gmail.com>
2016-10-21 12:39:50 -0700

    added Check for Google play services API and added foreground handling of notifications

--- Bumerang/app/src/main/java/com/seng480b/bumerang/MyFirebaseMessagingService.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/MyFirebaseMessagingService.java
@@ -1,6 +1,11 @@
 package com.seng480b.bumerang;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -10,11 +15,34 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
+        // Get from sender information from message
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        }
+
         // TODO(developer): Handle FCM messages here.
         // If the application is in the foreground handle both data and notification messages here.
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        Intent resultIntent = new Intent(this, Home.class);
+        resultIntent.putExtra("BorrowDialogFragment","borrowDialog");
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.bumerang_umbrella).setContentTitle("Bumerang").setContentTitle("Someone has accepted your request!");
+        mBuilder.setContentIntent(resultPendingIntent);
+        // Sets an ID for the notification
+        int mNotificationId = 001;
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Builds the notification and issues it
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+
     }
 }
