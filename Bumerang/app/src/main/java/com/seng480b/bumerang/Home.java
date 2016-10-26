ccee42adffe507e044567f900159f53ab8c01e35
Brady Schnell <bradyschnell@gmail.com>
2016-10-26 01:29:45 -0700

    In app heads up notifications are working!...out of app are not

--- Bumerang/app/src/main/java/com/seng480b/bumerang/Home.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/Home.java
@@ -39,52 +39,41 @@ public class Home extends AppCompatActivity
         //toolbar/actionbar setup
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        String borrowDialog = getIntent().getStringExtra("BorrowDialogFragment");
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        FragmentManager fragmentManager = getFragmentManager();
 
-        // If borrowDialog is defined then this activity was launched with a fragment selection
-        if (borrowDialog != null) {
-            if (borrowDialog.equals("borrowDialog")) {
-                Fragment my_requests = new MyRequests();
-                ft.replace(R.id.mainFrame, my_requests);;
-                ft.commit();
-            }
-        } else {
-            /**ADDED THIS**/
-            // Create the adapter that will return a fragment for each of the three
-            // primary sections of the activity.
-            mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-            // Set up the ViewPager with the sections adapter.
-            mViewPager = (ViewPager) findViewById(R.id.container);
-            mViewPager.setAdapter(mSectionsPagerAdapter);
-            tabLayout = (TabLayout) findViewById(R.id.tabs);
-            tabLayout.setupWithViewPager(mViewPager);
+        /**ADDED THIS**/
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
-            //Fragment Browse --> main page
+        //Fragment Browse --> main page
         /*ListFragment browse = new Browse();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.mainFrame,browse);
         ft.commit();*/
 
-            fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     floatingClicked();
                 }
             });
 
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-            drawer.setDrawerListener(toggle);
-            toggle.syncState();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-            navigationView.setNavigationItemSelectedListener(this);
-        }
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     public void floatingClicked() {
--- Bumerang/app/src/main/java/com/seng480b/bumerang/MyFirebaseMessagingService.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/MyFirebaseMessagingService.java
@@ -1,18 +1,23 @@
 package com.seng480b.bumerang;
 
 
+import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.WindowManager;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FBMS";
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
@@ -34,9 +39,20 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
         Intent resultIntent = new Intent(this, Home.class);
-        resultIntent.putExtra("BorrowDialogFragment","borrowDialog");
+        // Add information to the extra in order to specify action upon returning to home screen.
+        resultIntent.putExtra("MsgRecieved", "messageRecieved");
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.bumerang_umbrella).setContentTitle("Bumerang").setContentTitle("Someone has accepted your request!");
+
+        // Priority of the messages could be either MAX or HIGH. Set to max because they are time critical and urgent.
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.bumerang_umbrella).setContentTitle("Bumerang")
+                .setContentTitle("Someone has accepted your request!")
+                .setContentText("Someone has responded to you!")
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setPriority(Notification.PRIORITY_HIGH);
+
+
         mBuilder.setContentIntent(resultPendingIntent);
         // Sets an ID for the notification
         int mNotificationId = 001;
@@ -44,5 +60,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         // Builds the notification and issues it
         mNotifyMgr.notify(mNotificationId, mBuilder.build());
+
     }
 }
