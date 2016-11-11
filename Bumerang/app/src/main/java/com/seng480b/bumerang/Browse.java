0b87610631fef16fec8d022d0a8f975fb4b0f44f
dtempleman <daniel.templeman@gmail.com>
2016-11-10 17:18:52 -0800

    created OfferUtility.java, created an offer when clicking on the respond button in the DetailFragmeny.
    cleaned up the code for viewing DetailFragments

--- Bumerang/app/src/main/java/com/seng480b/bumerang/Browse.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/Browse.java
@@ -16,9 +16,6 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -149,43 +146,10 @@ public class Browse extends ListFragment implements OnItemClickListener {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                        //TODO: fix bug where it displays the wrong request
                         Request req = requests.get(position);
-
-                        String time = "Will expire in " + req.getMinutesUntilExpiry() + " minutes.";
-                        String itemName = req.getTitle();
-                        int userID = req.getUserId();
-
-                        UserDataCache.setRecentUser(null);
-                        ProfileUtility.storeRecentUserFromUserId(userID);
-
-                        Profile requestUser = UserDataCache.getRecentUser();;
-
-                        //TODO: this while loop must go!, it is only temporary
-                        boolean correctUser = false;
-                        while(requestUser==null){
-                            requestUser = UserDataCache.getRecentUser();
-                        }
-                        //TODO: it just continually checks to see if teh getRequest has finished
-
-
-                        String userName = requestUser.getFirstName();
-                        String desc = req.getDescription();
-                        String fb_id = Long.toString(requestUser.getFacebookId());
-
-                        JSONObject obj = new JSONObject();
-                        try {
-                            obj.put("Name", userName);
-                            obj.put("Item", itemName);
-                            obj.put("Exp", time);
-                            obj.put("FB_id", fb_id);
-                            obj.put("Description", desc);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
                         DetailFragment details = new DetailFragment();
-
-                        details.sendInfo(obj);
+                        details.setRequest(req);
 
                         FragmentManager fm = getFragmentManager();
                         details.show(fm,"Sample Fragment");
