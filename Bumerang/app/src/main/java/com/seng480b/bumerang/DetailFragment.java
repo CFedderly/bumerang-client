a7d8a579cd4032ae223264aebd26c9e32c6f1e6c
Brady Schnell <bradyschnell@gmail.com>
2016-11-16 13:06:05 -0800

    Fixed#109 where launching the app from a notification would cause no data to be loaded.

--- Bumerang/app/src/main/java/com/seng480b/bumerang/DetailFragment.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/DetailFragment.java
@@ -35,8 +35,6 @@ public class DetailFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Override
