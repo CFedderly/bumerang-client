6792af5136777028e733efee0f4f8b2c954063e6
connerleverett <connerleverett@gmail.com>
2016-11-22 18:00:22 -0800

    Usability / Post to FB bug (#123)
    * Beautified login page and modified nav bar
    
    * Fixed issue where if you didn't put a description it wouldn't post to FB
    
    Fixes#100

--- Bumerang/app/src/main/java/com/seng480b/bumerang/activities/MainActivity.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/activities/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Makes screen fullscreen
+        getSupportActionBar().hide();
         FacebookSdk.sdkInitialize(getApplicationContext());
 
 
