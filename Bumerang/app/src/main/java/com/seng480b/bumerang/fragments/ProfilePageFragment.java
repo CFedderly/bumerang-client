e8e9a6c43d5f13af6a8d7d827d48632ca03a80f8
Conner <connerleverett@gmail.com>
2016-11-26 17:46:07 -0800

    Added popup window to profile page to explain Karma system

--- Bumerang/app/src/main/java/com/seng480b/bumerang/fragments/ProfilePageFragment.java
+++ Bumerang/app/src/main/java/com/seng480b/bumerang/fragments/ProfilePageFragment.java
@@ -1,14 +1,20 @@
 package com.seng480b.bumerang.fragments;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
+
 import android.widget.TextView;
 
 import com.facebook.login.widget.ProfilePictureView;
@@ -18,8 +24,13 @@ import com.seng480b.bumerang.utils.Utility;
 import com.seng480b.bumerang.activities.HomeActivity;
 import com.seng480b.bumerang.models.Profile;
 
+
 public class ProfilePageFragment extends Fragment {
     private View myInflatedView;
+    private PopupWindow popupWindow;
+    private LayoutInflater layoutInflater;
+    private int width;
+    private int height;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myInflatedView = inflater.inflate(R.layout.fragment_profile_page, container,false);
@@ -34,7 +45,8 @@ public class ProfilePageFragment extends Fragment {
         if (UserDataCache.hasProfile()) {
             updateProfileInfo();
         }
-        setUpButton();
+        setUpEditButton();
+        setupKarmaButton();
         return myInflatedView;
     }
 
@@ -61,7 +73,7 @@ public class ProfilePageFragment extends Fragment {
         }
     }
 
-    public void setUpButton(){
+    public void setUpEditButton(){
         Button editButton = (Button) myInflatedView.findViewById(R.id.profileButtonEdit);
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,4 +89,42 @@ public class ProfilePageFragment extends Fragment {
 
     }
 
+    public void setupKarmaButton(){
+        //This is getting the size of the phone so that the popup is displayed in the middle of the page
+        Display display =getActivity().getWindowManager().getDefaultDisplay();
+        width = display.getWidth();
+        height = display.getHeight();
+        Button karmaButton = (Button) myInflatedView.findViewById(R.id.profileButtonKarmaHelp);
+
+        karmaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutInflater = (LayoutInflater) getActivity().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.fragment_profile_karma_explanation,null);
+
+                popupWindow = new PopupWindow(container, 750, 640,true);
+                popupWindow.showAtLocation(myInflatedView, Gravity.NO_GRAVITY, width/6, height/3);
+                myInflatedView.setAlpha(0.5F);
+
+                popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener(){
+                    @Override
+                    public void onDismiss() {
+                        myInflatedView.setAlpha(1F);
+                    }
+                });
+
+                //This makes it so that if you click on the popup it will also disappear
+                container.setOnTouchListener(new View.OnTouchListener(){
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        myInflatedView.setAlpha(1F);
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+
+            }
+
+        });
+    }
 }
